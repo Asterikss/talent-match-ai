20edfe9da693d2bad22d78a3fe645b5331d068eb
Asterikss <AndreAndreM@proton.me>
2026-01-08 23:56:10 +0100

    feat: add master router

---
+++ backend/src/api/v1/master_router.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from api.v1.endpoints.entities import router as entities_router
+from api.v1.endpoints.info import router as info_router
+from api.v1.endpoints.ingest import router as ingest_router
+from api.v1.endpoints.query import router as query_router
+from core.config import config
+
+rounter = APIRouter(prefix=config.API_V1_STR)
+
+rounter.include_router(entities_router, tags=["Get Entities Operations"])
+rounter.include_router(info_router, tags=["Info Operations"])
+rounter.include_router(ingest_router, tags=["Ingest Operations"])
+rounter.include_router(query_router, tags=["Query Operations"])
